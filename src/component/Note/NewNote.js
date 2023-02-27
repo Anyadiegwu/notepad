81878623f4fe73b4d121d628bb2d36ca87d5a3ef
Anyadiegwu <victoranyadiegwu610@gmail.com>
2023-02-27 23:05:51 +0100

    deployed

--- src/component/Note/NewNote.js
+++ src/component/Note/NewNote.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import {  useState } from 'react';
 import styles from './Change.module.css'
 
 
@@ -26,23 +26,59 @@ function NewNote ({handleUpdateNote, handleReadFile, handleFontSize}){
         setTextNote([File.prototype.name, File.prototype.type])
         handleReadFile(Blob.prototype.stream()) 
     }
+    const handleSelect = (e) => { 
+    const elements = document.querySelectorAll(".btn");
+    elements.forEach(element =>{
+        element.addEventListener("click", () =>{
+            let key = Number(elements)
+            
+            if(key > 0 && key < 2){
+               console.log( <p contentEditable={{style: {fontWeight: "700px"}}}>{e}</p>)
+            }else if(key > 1 && key < 3){
+                <p contentEditable={{style: {fontStyle: "italic"}}}>{e}</p>
+            }
+        
+            // let command = element.dataset['element']
 
- 
+            // key.execCommand(command, false, null)
+           
+        })
+    })}
+    // const handleSelect = (e) => {
+    //     format();
+    //     return <p key={e.nanoid()}></p>
+  
+    // }
+  
     return(
         <>
             <div className={styles.notebook} id={styles.new}>
                 <textarea
-                    rows='6'
+                    rows='8'
                     cols="10"
                     placeholder='Type to add a new note here..'
                     value={textNote}
                     onChange={handleChange}
+                    onSelect={handleSelect}
+                    contentEditable={{style: {}}}
                 ></textarea>
-                <input type="file" onChange={handleChangeFile}/>
+                <div  className={styles.input}>
+                <input type="file" 
+                onChange={handleChangeFile}/>
+                </div>
                 <div className={styles.datetag}>
                     <small>
-                        <select className="format">Format text
-                            <option onClick={() => 
+                        <button 
+                        className={styles.btn}
+                        data-elemente="bold">B</button>
+                        <button 
+                        className={styles.btn}
+                        data-element="italic">I</button>
+                        <button 
+                        className={styles.btn}
+                        data-element="underline">U</button>
+                        {/* <select className="format">Format text */}
+                            {/* <option onClick={() => 
                                 handleFontSize((previousFontSize) => !previousFontSize)}
                                 className="format1">Bold</option>
                             <option onClick={() => 
@@ -50,10 +86,25 @@ function NewNote ({handleUpdateNote, handleReadFile, handleFontSize}){
                                 className="format2">Italics</option>
                             <option onClick={() => 
                                 handleFontSize((previousFontSize) => !previousFontSize)}
-                                className="format3">Underline</option>
-                        </select>
+                                className="format3">Underline</option> */}
+                        {/* </select> */}
                         <select className="size">Font-Size
-                            <option onClick={() => 
+                            <option>
+                                <button 
+                                className={styles.btn}
+                                datatype="size1">12</button>
+                            </option>
+                            <option>
+                                <button 
+                                className={styles.btn}
+                                datatype="size2">14</button>
+                            </option>
+                            <option>
+                                <button 
+                                className={styles.btn}
+                                datatype="size3">16</button>
+                            </option>
+                            {/* <option onClick={() => 
                                 handleFontSize((previousFontSize) => !previousFontSize)}
                                 className="size1">12</option>
                             <option onClick={() => 
@@ -61,7 +112,7 @@ function NewNote ({handleUpdateNote, handleReadFile, handleFontSize}){
                                 className="size2">14</option>
                             <option onClick={() => 
                                 handleFontSize((previousFontSize) => !previousFontSize)}
-                                className="size3">16</option>
+                                className="size3">16</option> */}
                         </select>
                     </small>
                     <button className={styles.save} onClick={handleSubmit}>Save</button>
